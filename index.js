81d3b2221400cbed677c446682ef1a2d479b86d8
beccarusso <beccacrusso@icloud.com>
2023-05-02 07:39:53 -0400

    Added emoji hover feature on homepage

---
+++ index.js
@@ -0,0 +1,13 @@
+// Get emojis from https://emojipedia.org/
+
+const btn = document.getElementById("emoji-btn");
+const emojis = [ 
+     "😍",  "💖",   "🧡",   "💛",   "💚",   "💙",
+     "💜",  "🌸",   "🌺",   "🌼",   "🌻",   "☮",
+     "🌱",  "🌎",   "🍃",   "🌙",   "✨",   "🌟",
+     "💕"
+];
+
+btn.addEventListener("mouseover", () => {
+  btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+});
