b2ba0b86942438462f1166d758a829624a15d705
beccarusso <beccacrusso@icloud.com>
2023-12-17 21:12:22 -0500

    Added click functionality for emoji on homepage to be clickable (for mobile viewers accessibility)

--- JS/index.js
+++ JS/index.js
@@ -26,3 +26,7 @@ const emojis = [
 btn.addEventListener("mouseover", () => {
   btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
 });
+
+btn.addEventListener("click", () => {
+  btn.innerText = emojis[Math.floor(Math.random() * emojis.length)];
+});
